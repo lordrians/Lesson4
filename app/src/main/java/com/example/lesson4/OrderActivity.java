0c6e3095d3560db45fb287be8f731daf10ba0a0c
ferrian <ferrianrp@gmail.com>
2020-03-25 02:16:44 +0700

    tambah submit

--- app/src/main/java/com/example/lesson4/OrderActivity.java
+++ app/src/main/java/com/example/lesson4/OrderActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity implements View.OnClickListener {
-    String pOrderan;
-    TextView tvTerima;
+    String  nama, address, phone, note;
+    TextView tvTerima, tvSubNama, tvSubAddress, tvSubPhone, tvSubNote, tvSubChoose;
     EditText etNama, etAddress, etPhone, etNote;
+    Button btnSubmit;
+    RadioGroup rgDeliv;
+    RadioButton r;
 
     RadioButton rbSame, rbNext, rbPickup;
 
@@ -30,28 +35,69 @@ public class OrderActivity extends AppCompatActivity implements View.OnClickList
         rbPickup = findViewById(R.id.rb_pickup);
         rbSame = findViewById(R.id.rb_same);
 
+        rgDeliv = findViewById(R.id.radioGroup);
+
+        btnSubmit = findViewById(R.id.btn_submit);
+
+        tvSubNama = findViewById(R.id.tv_submit_nama);
+        tvSubPhone = findViewById(R.id.tv_submit_phone);
+        tvSubAddress = findViewById(R.id.tv_submit_address);
+        tvSubNote = findViewById(R.id.tv_submit_note);
+        tvSubChoose = findViewById(R.id.tv_submit_deliv);
+
         tvTerima.setText(getIntent().getStringExtra("orderan"));
 
+        btnSubmit.setOnClickListener(this);
         rbSame.setOnClickListener(this);
         rbPickup.setOnClickListener(this);
         rbNext.setOnClickListener(this);
 
     }
 
+
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.rb_next:
-                Toast.makeText(getApplicationContext(), "Next day ground delivery", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Next day ground delivery", Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.rb_same:
-                Toast.makeText(getApplicationContext(), "Same day messenger service", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Same day messenger service", Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.rb_pickup:
-                Toast.makeText(getApplicationContext(), "Pickup", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Pickup", Toast.LENGTH_SHORT).show();
                 break;
+
+            case R.id.btn_submit:
+                nama = etNama.getText().toString();
+                address = etAddress.getText().toString();
+                phone = etPhone.getText().toString();
+                note = etNote.getText().toString();
+
+                int radioButtonID = rgDeliv.getCheckedRadioButtonId();
+                View radioButton = rgDeliv.findViewById(radioButtonID);
+                int idButton = rgDeliv.indexOfChild(radioButton);
+                r = (RadioButton) rgDeliv.getChildAt(idButton);
+
+                if (nama.isEmpty() && address.isEmpty() && phone.isEmpty() && note.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Tolong isi semua pertanyaan", Toast.LENGTH_LONG).show();
+                }else {
+                    if (rgDeliv.getCheckedRadioButtonId() == -1){
+                        Toast.makeText(getApplicationContext(), "Tolong isi metode pengiriman", Toast.LENGTH_LONG).show();
+                    }else {
+                        tvSubNama.setText(etNama.getText().toString());
+                        tvSubAddress.setText(etAddress.getText().toString());
+                        tvSubPhone.setText(etPhone.getText().toString());
+                        tvSubNote.setText(etNote.getText().toString());
+                        tvSubChoose.setText(r.getText().toString());
+                    }
+                }
+
+
+
+
         }
     }
 }
